55c6010ad2c98069f9db49f6cfb1a69c3a49ee1a
SignpostMarv <me@signpostmarv.name>
2020-03-07 23:36:34 +0000

    adjusting indentation

--- src/data/albums/OCRA-0025.ts
+++ src/data/albums/OCRA-0025.ts
@@ -32,72 +32,72 @@ export const OCRA0025: Album = {
 	},
 	discs: {
 		'': [
-		{
-			subpath: '/MP3/01 A, B, C, Start!.mp3',
-			name: 'A, B, C, Start!',
-			index: 1,
-		},
-		{
-			subpath: '/MP3/02 The Sound of Speed.mp3',
-			name: 'The Sound of Speed',
-			index: 2,
-		},
-		{
-			subpath: '/MP3/03 Shifting Islands.mp3',
-			name: 'Shifting Islands',
-			index: 3,
-		},
-		{
-			subpath: '/MP3/04 Subsonic Sparkle.mp3',
-			name: 'Subsonic Sparkle',
-			index: 4,
-		},
-		{
-			subpath: '/MP3/05 Spring Junkie.mp3',
-			name: 'Spring Junkie',
-			index: 5,
-		},
-		{
-			subpath: '/MP3/06 Bubble Junkie.mp3',
-			name: 'Bubble Junkie',
-			index: 6,
-		},
-		{
-			subpath: '/MP3/07 Fifty Rings to Ride.mp3',
-			name: 'Fifty Rings to Ride',
-			index: 7,
-		},
-		{
-			subpath: '/MP3/08 Under Construction.mp3',
-			name: 'Under Construction',
-			index: 8,
-		},
-		{
-			subpath: '/MP3/09 Hogtied.mp3',
-			name: 'Hogtied',
-			index: 9,
-		},
-		{
-			subpath: '/MP3/10 Caos.mp3',
-			name: 'Caos',
-			index: 10,
-		},
-		{
-			subpath: '/MP3/11 Clockwork Criminal.mp3',
-			name: 'Clockwork Criminal',
-			index: 11,
-		},
-		{
-			subpath: '/MP3/12 Final Progression.mp3',
-			name: 'Final Progression',
-			index: 12,
-		},
-		{
-			subpath: '/MP3/13 A Hog in His Prime.mp3',
-			name: 'A Hog in His Prime',
-			index: 13,
-		},
-	],
+			{
+				subpath: '/MP3/01 A, B, C, Start!.mp3',
+				name: 'A, B, C, Start!',
+				index: 1,
+			},
+			{
+				subpath: '/MP3/02 The Sound of Speed.mp3',
+				name: 'The Sound of Speed',
+				index: 2,
+			},
+			{
+				subpath: '/MP3/03 Shifting Islands.mp3',
+				name: 'Shifting Islands',
+				index: 3,
+			},
+			{
+				subpath: '/MP3/04 Subsonic Sparkle.mp3',
+				name: 'Subsonic Sparkle',
+				index: 4,
+			},
+			{
+				subpath: '/MP3/05 Spring Junkie.mp3',
+				name: 'Spring Junkie',
+				index: 5,
+			},
+			{
+				subpath: '/MP3/06 Bubble Junkie.mp3',
+				name: 'Bubble Junkie',
+				index: 6,
+			},
+			{
+				subpath: '/MP3/07 Fifty Rings to Ride.mp3',
+				name: 'Fifty Rings to Ride',
+				index: 7,
+			},
+			{
+				subpath: '/MP3/08 Under Construction.mp3',
+				name: 'Under Construction',
+				index: 8,
+			},
+			{
+				subpath: '/MP3/09 Hogtied.mp3',
+				name: 'Hogtied',
+				index: 9,
+			},
+			{
+				subpath: '/MP3/10 Caos.mp3',
+				name: 'Caos',
+				index: 10,
+			},
+			{
+				subpath: '/MP3/11 Clockwork Criminal.mp3',
+				name: 'Clockwork Criminal',
+				index: 11,
+			},
+			{
+				subpath: '/MP3/12 Final Progression.mp3',
+				name: 'Final Progression',
+				index: 12,
+			},
+			{
+				subpath: '/MP3/13 A Hog in His Prime.mp3',
+				name: 'A Hog in His Prime',
+				index: 13,
+			},
+		],
 	},
 };
 
--- src/data/albums/OCRA-0029.ts
+++ src/data/albums/OCRA-0029.ts
@@ -36,97 +36,97 @@ export const OCRA0029: Album = {
 	},
 	discs: {
 		'': [
-		{
-			subpath: '/MP3/01 Disasterpeace - Chamber of the Goddess [A Link to the Past].mp3',
-			name: '01 Disasterpeace - Chamber of the Goddess [A Link to the Past]',
-			index: 1,
-		},
-		{
-			subpath: '/MP3/02 Rekcahdam - Gimme My Sword! [Link\'s Awakening - A Link to the Past].mp3',
-			name: '02 Rekcahdam - Gimme My Sword! [Link\'s Awakening - A Link to the Past]',
-			index: 2,
-		},
-		{
-			subpath: '/MP3/03 Laura Shigihara - Fushigina Forest [A Link to the Past].mp3',
-			name: '03 Laura Shigihara - Fushigina Forest [A Link to the Past]',
-			index: 3,
-		},
-		{
-			subpath: '/MP3/04 Joshua Morse - Link\'s Epoch [A Link to the Past].mp3',
-			name: '04 Joshua Morse - Link\'s Epoch [A Link to the Past]',
-			index: 4,
-		},
-		{
-			subpath: '/MP3/05 Jeff Ball - Labyrinth of Dance Floors [A Link to the Past].mp3',
-			name: '05 Jeff Ball - Labyrinth of Dance Floors [A Link to the Past]',
-			index: 5,
-		},
-		{
-			subpath: '/MP3/06 HyperDuck SoundWorks - Hoy, Small Fry! [Wind Waker - Ocarina of Time].mp3',
-			name: '06 HyperDuck SoundWorks - Hoy, Small Fry! [Wind Waker - Ocarina of Time]',
-			index: 6,
-		},
-		{
-			subpath: '/MP3/07 Air & Sea - To Everything There Is a Temple of Seasons [Oracle of Seasons].mp3',
-			name: '07 Air & Sea - To Everything There Is a Temple of Seasons [Oracle of Seasons]',
-			index: 7,
-		},
-		{
-			subpath: '/MP3/08 C418 - Skyward [Skyward Sword].mp3',
-			name: '08 C418 - Skyward [Skyward Sword]',
-			index: 8,
-		},
-		{
-			subpath: '/MP3/09 Big Giant Circles feat. Jeff Ball - Thunderstruck [Ocarina of Time - Legend of Zelda].mp3',
-			name: '09 Big Giant Circles feat. Jeff Ball - Thunderstruck [Ocarina of Time - Legend of Zelda]',
-			index: 9,
-		},
-		{
-			subpath: '/MP3/10 Josh Whelchel - Zelda\'s First Trip to the \'Village\' [OOT - LTTP - LOZ].mp3',
-			name: '10 Josh Whelchel - Zelda\'s First Trip to the \'Village\' [OOT - LTTP - LOZ]',
-			index: 10,
-		},
-		{
-			subpath: '/MP3/11 MisfitChris - Village from Your Past [Ocarina of Time].mp3',
-			name: '11 MisfitChris - Village from Your Past [Ocarina of Time]',
-			index: 11,
-		},
-		{
-			subpath: '/MP3/12 Mattias Haggstrom Gerdt - Hey, Listen [Ocarina of Time - Legend of Zelda].mp3',
-			name: '12 Mattias Haggstrom Gerdt - Hey, Listen [Ocarina of Time - Legend of Zelda]',
-			index: 12,
-		},
-		{
-			subpath: '/MP3/13 Kozilek - Last Dance of the Giants [Majora\'s Mask].mp3',
-			name: '13 Kozilek - Last Dance of the Giants [Majora\'s Mask]',
-			index: 13,
-		},
-		{
-			subpath: '/MP3/14 Gryzor87 - Ilia\'s Adagio Meets Dark March [Twilight Princess - A Link to the Past].mp3',
-			name: '14 Gryzor87 - Ilia\'s Adagio Meets Dark March [Twilight Princess - A Link to the Past]',
-			index: 14,
-		},
-		{
-			subpath: '/MP3/15 Dong - Ballad of the Wind Fish (Kaze no Sakana Mix) [Link\'s Awakening].mp3',
-			name: '15 Dong - Ballad of the Wind Fish (Kaze no Sakana Mix) [Link\'s Awakening]',
-			index: 15,
-		},
-		{
-			subpath: '/MP3/16 CTPLR - Lon Lon Ranch (CTPLR Mix) [Ocarina of Time].mp3',
-			name: '16 CTPLR - Lon Lon Ranch (CTPLR Mix) [Ocarina of Time]',
-			index: 16,
-		},
-		{
-			subpath: '/MP3/17 SoulEye - Link\'s Final Battle [Link\'s Adventure].mp3',
-			name: '17 SoulEye - Link\'s Final Battle [Link\'s Adventure]',
-			index: 17,
-		},
-		{
-			subpath: '/MP3/18 Matheus Manente - Zelda\'s Graceful Nightmare [OOT - MM - WW].mp3',
-			name: '18 Matheus Manente - Zelda\'s Graceful Nightmare [OOT - MM - WW]',
-			index: 18,
-		},
-	],
+			{
+				subpath: '/MP3/01 Disasterpeace - Chamber of the Goddess [A Link to the Past].mp3',
+				name: '01 Disasterpeace - Chamber of the Goddess [A Link to the Past]',
+				index: 1,
+			},
+			{
+				subpath: '/MP3/02 Rekcahdam - Gimme My Sword! [Link\'s Awakening - A Link to the Past].mp3',
+				name: '02 Rekcahdam - Gimme My Sword! [Link\'s Awakening - A Link to the Past]',
+				index: 2,
+			},
+			{
+				subpath: '/MP3/03 Laura Shigihara - Fushigina Forest [A Link to the Past].mp3',
+				name: '03 Laura Shigihara - Fushigina Forest [A Link to the Past]',
+				index: 3,
+			},
+			{
+				subpath: '/MP3/04 Joshua Morse - Link\'s Epoch [A Link to the Past].mp3',
+				name: '04 Joshua Morse - Link\'s Epoch [A Link to the Past]',
+				index: 4,
+			},
+			{
+				subpath: '/MP3/05 Jeff Ball - Labyrinth of Dance Floors [A Link to the Past].mp3',
+				name: '05 Jeff Ball - Labyrinth of Dance Floors [A Link to the Past]',
+				index: 5,
+			},
+			{
+				subpath: '/MP3/06 HyperDuck SoundWorks - Hoy, Small Fry! [Wind Waker - Ocarina of Time].mp3',
+				name: '06 HyperDuck SoundWorks - Hoy, Small Fry! [Wind Waker - Ocarina of Time]',
+				index: 6,
+			},
+			{
+				subpath: '/MP3/07 Air & Sea - To Everything There Is a Temple of Seasons [Oracle of Seasons].mp3',
+				name: '07 Air & Sea - To Everything There Is a Temple of Seasons [Oracle of Seasons]',
+				index: 7,
+			},
+			{
+				subpath: '/MP3/08 C418 - Skyward [Skyward Sword].mp3',
+				name: '08 C418 - Skyward [Skyward Sword]',
+				index: 8,
+			},
+			{
+				subpath: '/MP3/09 Big Giant Circles feat. Jeff Ball - Thunderstruck [Ocarina of Time - Legend of Zelda].mp3',
+				name: '09 Big Giant Circles feat. Jeff Ball - Thunderstruck [Ocarina of Time - Legend of Zelda]',
+				index: 9,
+			},
+			{
+				subpath: '/MP3/10 Josh Whelchel - Zelda\'s First Trip to the \'Village\' [OOT - LTTP - LOZ].mp3',
+				name: '10 Josh Whelchel - Zelda\'s First Trip to the \'Village\' [OOT - LTTP - LOZ]',
+				index: 10,
+			},
+			{
+				subpath: '/MP3/11 MisfitChris - Village from Your Past [Ocarina of Time].mp3',
+				name: '11 MisfitChris - Village from Your Past [Ocarina of Time]',
+				index: 11,
+			},
+			{
+				subpath: '/MP3/12 Mattias Haggstrom Gerdt - Hey, Listen [Ocarina of Time - Legend of Zelda].mp3',
+				name: '12 Mattias Haggstrom Gerdt - Hey, Listen [Ocarina of Time - Legend of Zelda]',
+				index: 12,
+			},
+			{
+				subpath: '/MP3/13 Kozilek - Last Dance of the Giants [Majora\'s Mask].mp3',
+				name: '13 Kozilek - Last Dance of the Giants [Majora\'s Mask]',
+				index: 13,
+			},
+			{
+				subpath: '/MP3/14 Gryzor87 - Ilia\'s Adagio Meets Dark March [Twilight Princess - A Link to the Past].mp3',
+				name: '14 Gryzor87 - Ilia\'s Adagio Meets Dark March [Twilight Princess - A Link to the Past]',
+				index: 14,
+			},
+			{
+				subpath: '/MP3/15 Dong - Ballad of the Wind Fish (Kaze no Sakana Mix) [Link\'s Awakening].mp3',
+				name: '15 Dong - Ballad of the Wind Fish (Kaze no Sakana Mix) [Link\'s Awakening]',
+				index: 15,
+			},
+			{
+				subpath: '/MP3/16 CTPLR - Lon Lon Ranch (CTPLR Mix) [Ocarina of Time].mp3',
+				name: '16 CTPLR - Lon Lon Ranch (CTPLR Mix) [Ocarina of Time]',
+				index: 16,
+			},
+			{
+				subpath: '/MP3/17 SoulEye - Link\'s Final Battle [Link\'s Adventure].mp3',
+				name: '17 SoulEye - Link\'s Final Battle [Link\'s Adventure]',
+				index: 17,
+			},
+			{
+				subpath: '/MP3/18 Matheus Manente - Zelda\'s Graceful Nightmare [OOT - MM - WW].mp3',
+				name: '18 Matheus Manente - Zelda\'s Graceful Nightmare [OOT - MM - WW]',
+				index: 18,
+			},
+		],
 	},
 };
 
