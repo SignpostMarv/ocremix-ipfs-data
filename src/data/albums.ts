e95776bc2f51e8114b134dcc04e326a4fdd5db38
SignpostMarv <me@signpostmarv.name>
2020-03-08 16:50:26 +0000

    correcting typos

--- src/data/albums.ts
+++ src/data/albums.ts
@@ -3,17 +3,17 @@ import {
 } from '../module';
 
 export const Albums: {[id: string]: () => Promise<Album>} = {
-	OCRA006: async (): Promise<Album> => {
+	OCRA0006: async (): Promise<Album> => {
 		const {OCRA0006} = await import('./albums/OCRA-0006.js');
 
 		return OCRA0006;
 	},
-	OCRA025: async (): Promise<Album> => {
+	OCRA0025: async (): Promise<Album> => {
 		const {OCRA0025} = await import('./albums/OCRA-0025.js');
 
 		return OCRA0025;
 	},
-	OCRA029: async (): Promise<Album> => {
+	OCRA0029: async (): Promise<Album> => {
 		const {OCRA0029} = await import('./albums/OCRA-0029.js');
 
 		return OCRA0029;
