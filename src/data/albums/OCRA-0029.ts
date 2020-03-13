f3f19d71965ded48933ea6ba559504af6d1b5695
SignpostMarv <me@signpostmarv.name>
2020-03-13 00:11:45 +0000

    removing track number / arranger from track name
    transcribing the credit data to the album makes listing the credit in
    the track name redundant.

--- src/data/albums/OCRA-0029.ts
+++ src/data/albums/OCRA-0029.ts
@@ -77,7 +77,7 @@ export const OCRA0029: AlbumWithArt = {
 		'': [
 			{
 				subpath: '/MP3/01 Disasterpeace - Chamber of the Goddess [A Link to the Past].mp3',
-				name: '01 Disasterpeace - Chamber of the Goddess [A Link to the Past]',
+				name: 'Chamber of the Goddess [A Link to the Past]',
 				index: 1,
 				credits: [
 					disasterpeace,
@@ -85,7 +85,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/02 Rekcahdam - Gimme My Sword! [Link\'s Awakening - A Link to the Past].mp3',
-				name: '02 Rekcahdam - Gimme My Sword! [Link\'s Awakening - A Link to the Past]',
+				name: 'Gimme My Sword! [Link\'s Awakening - A Link to the Past]',
 				index: 2,
 				credits: [
 					rekcahdam,
@@ -93,7 +93,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/03 Laura Shigihara - Fushigina Forest [A Link to the Past].mp3',
-				name: '03 Laura Shigihara - Fushigina Forest [A Link to the Past]',
+				name: 'Fushigina Forest [A Link to the Past]',
 				index: 3,
 				credits: [
 					laurashigihara,
@@ -101,7 +101,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/04 Joshua Morse - Link\'s Epoch [A Link to the Past].mp3',
-				name: '04 Joshua Morse - Link\'s Epoch [A Link to the Past]',
+				name: 'Link\'s Epoch [A Link to the Past]',
 				index: 4,
 				credits: [
 					joshuamorse,
@@ -109,7 +109,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/05 Jeff Ball - Labyrinth of Dance Floors [A Link to the Past].mp3',
-				name: '05 Jeff Ball - Labyrinth of Dance Floors [A Link to the Past]',
+				name: 'Labyrinth of Dance Floors [A Link to the Past]',
 				index: 5,
 				credits: [
 					jeffball,
@@ -117,7 +117,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/06 HyperDuck SoundWorks - Hoy, Small Fry! [Wind Waker - Ocarina of Time].mp3',
-				name: '06 HyperDuck SoundWorks - Hoy, Small Fry! [Wind Waker - Ocarina of Time]',
+				name: 'Hoy, Small Fry! [Wind Waker - Ocarina of Time]',
 				index: 6,
 				credits: [
 					hyperducksoundworks,
@@ -125,7 +125,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/07 Air & Sea - To Everything There Is a Temple of Seasons [Oracle of Seasons].mp3',
-				name: '07 Air & Sea - To Everything There Is a Temple of Seasons [Oracle of Seasons]',
+				name: 'To Everything There Is a Temple of Seasons [Oracle of Seasons]',
 				index: 7,
 				credits: [
 					{
@@ -136,7 +136,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/08 C418 - Skyward [Skyward Sword].mp3',
-				name: '08 C418 - Skyward [Skyward Sword]',
+				name: 'Skyward [Skyward Sword]',
 				index: 8,
 				credits: [
 					c418,
@@ -144,7 +144,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/09 Big Giant Circles feat. Jeff Ball - Thunderstruck [Ocarina of Time - Legend of Zelda].mp3',
-				name: '09 Big Giant Circles feat. Jeff Ball - Thunderstruck [Ocarina of Time - Legend of Zelda]',
+				name: 'Thunderstruck [Ocarina of Time - Legend of Zelda]',
 				index: 9,
 				credits: [
 					biggiantcircles,
@@ -153,7 +153,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/10 Josh Whelchel - Zelda\'s First Trip to the \'Village\' [OOT - LTTP - LOZ].mp3',
-				name: '10 Josh Whelchel - Zelda\'s First Trip to the \'Village\' [OOT - LTTP - LOZ]',
+				name: 'Zelda\'s First Trip to the \'Village\' [OOT - LTTP - LOZ]',
 				index: 10,
 				credits: [
 					joshwhelchel,
@@ -161,7 +161,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/11 MisfitChris - Village from Your Past [Ocarina of Time].mp3',
-				name: '11 MisfitChris - Village from Your Past [Ocarina of Time]',
+				name: 'Village from Your Past [Ocarina of Time]',
 				index: 11,
 				credits: [
 					{
@@ -172,7 +172,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/12 Mattias Haggstrom Gerdt - Hey, Listen [Ocarina of Time - Legend of Zelda].mp3',
-				name: '12 Mattias Haggstrom Gerdt - Hey, Listen [Ocarina of Time - Legend of Zelda]',
+				name: 'Hey, Listen [Ocarina of Time - Legend of Zelda]',
 				index: 12,
 				credits: [
 					mattiashaggstromgerdt,
@@ -180,7 +180,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/13 Kozilek - Last Dance of the Giants [Majora\'s Mask].mp3',
-				name: '13 Kozilek - Last Dance of the Giants [Majora\'s Mask]',
+				name: 'Last Dance of the Giants [Majora\'s Mask]',
 				index: 13,
 				credits: [
 					{
@@ -191,7 +191,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/14 Gryzor87 - Ilia\'s Adagio Meets Dark March [Twilight Princess - A Link to the Past].mp3',
-				name: '14 Gryzor87 - Ilia\'s Adagio Meets Dark March [Twilight Princess - A Link to the Past]',
+				name: 'Ilia\'s Adagio Meets Dark March [Twilight Princess - A Link to the Past]',
 				index: 14,
 				credits: [
 					{
@@ -202,7 +202,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/15 Dong - Ballad of the Wind Fish (Kaze no Sakana Mix) [Link\'s Awakening].mp3',
-				name: '15 Dong - Ballad of the Wind Fish (Kaze no Sakana Mix) [Link\'s Awakening]',
+				name: 'Ballad of the Wind Fish (Kaze no Sakana Mix) [Link\'s Awakening]',
 				index: 15,
 				credits: [
 					dong,
@@ -210,7 +210,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/16 CTPLR - Lon Lon Ranch (CTPLR Mix) [Ocarina of Time].mp3',
-				name: '16 CTPLR - Lon Lon Ranch (CTPLR Mix) [Ocarina of Time]',
+				name: 'Lon Lon Ranch (CTPLR Mix) [Ocarina of Time]',
 				index: 16,
 				credits: [
 					{
@@ -221,7 +221,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/17 SoulEye - Link\'s Final Battle [Link\'s Adventure].mp3',
-				name: '17 SoulEye - Link\'s Final Battle [Link\'s Adventure]',
+				name: 'Link\'s Final Battle [Link\'s Adventure]',
 				index: 17,
 				credits: [
 					{
@@ -232,7 +232,7 @@ export const OCRA0029: AlbumWithArt = {
 			},
 			{
 				subpath: '/MP3/18 Matheus Manente - Zelda\'s Graceful Nightmare [OOT - MM - WW].mp3',
-				name: '18 Matheus Manente - Zelda\'s Graceful Nightmare [OOT - MM - WW]',
+				name: 'Zelda\'s Graceful Nightmare [OOT - MM - WW]',
 				index: 18,
 				credits: [
 					matheusmanente,
